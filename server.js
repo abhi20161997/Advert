862e726b317543c1c0ce4578a7dd45ee66227d96
Harshit Shukla <Harshitshukla151001@gmail.com>
2021-05-06 19:52:15 +0530

    push

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const path = require("path");
 const app = express();
 
 app.use(compression());
-app.use("/getintouch", express.static(path.join(__dirname, "public")));
+app.use("/register", express.static(path.join(__dirname, "public")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "getintouch.html"));
+app.get("/register/getintouch", function (req, res) {
+  res.sendFile(path.join(__dirname, "getintouch.html"));
+});
+
+app.get("/register/get-in-touch", function (req, res) {
+  res.sendFile(path.join(__dirname, "get-in-touch.html"));
+});
+
+app.get("/register/get_in_touch", function (req, res) {
+  res.sendFile(path.join(__dirname, "get_in_touch.html"));
+});
+
+app.get("/register/thankyou", function (req, res) {
+  res.sendFile(path.join(__dirname, "thankyou.html"));
 });
 
 const PORT = process.env.PORT || 4444;
